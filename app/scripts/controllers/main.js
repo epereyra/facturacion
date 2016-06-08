cc8af30bc47a38943fb1fa16cad30773d27407bc
Sergio Alejandro Lucero <selucero@syc.dom>
2016-06-08 15:26:34 -0300

    Incrementar cantidad del detalle

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,29 +11,47 @@ angular.module('proyectoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.nombre = '';
     $scope.productos = [];
-    $scope.factura = [];
+    $scope.items = [];
+
+    var id = 0;
+
+    var obtenerId = function() {
+      return id++;
+    }
 
     $scope.agregarProducto = function(nombre) {
       console.log(nombre);
-      $scope.productos.push(nombre);
+      let producto = {
+        id: obtenerId(),
+        nombre: nombre,
+        precio: Math.round(Math.random() * (30 - 5) + 5),
+        categoria: '',
+        stock: 0
+      };
+      $scope.productos.push(producto);
       $scope.nombre = '';
     }
 
-    $scope.agregarItem = function(nombre) {
-      $scope.item = {
-        id: 0,
-        descripcion: nombre,
-        cantidad: 0,
-        precio: 15
-      };
+    $scope.agregarItem = function(producto) {
 
-      console.log($scope.item);
-      $scope.factura.push($scope.item);
-      $scope.item = {};
+      //console.log('agregarItem %O', producto);
+      var position = $scope.productos.findIndex(function(elemento, indice, productos) {
+        return elemento.id === producto.id;
+      });
+
+
+      $scope.productos[position].cantidad = 1;
+      $scope.items.push($scope.productos[position]);
+      console.log('Producto agregado: %O',$scope.productos[position]);
     }
 
     $scope.incrementarCantidad = function (id) {
+      console.log(id);
       // encontrar el indice del arreglo factura que corresponde a id
+      var position = $scope.items.findIndex(function(elemento, indice, items) {
+        return elemento.id === id;
+      });
       // actualizar la cantidad del arreglo factura en esa posicion
+      $scope.items[position].cantidad++;
     };
   });
